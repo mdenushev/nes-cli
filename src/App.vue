113bd6865bcacd9c4df3b647cc406d324f354ade
Max Denushev <mdenushev@ya.ru>
2019-12-17 19:25:43 +0700

    1

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,84 @@
 <template>
   <div id="app" class="container">
-    <b-row>
-      <b-form-input v-model="host" type="url" placeholder="Host" class="col-6" :state="hostState"></b-form-input>
+    <b-row align-v="center">
+      <b-form-input v-model="host" type="url" placeholder="Host" class="col-6" :state="hostState">Host</b-form-input>
       <b-form-checkbox v-model="auth" class="col-2">Authorization</b-form-checkbox>
-      <b-button class="col-4" variant="primary">Connect</b-button>
+      <b-button class="col-4" variant="primary" @click="nesConnect">Connect</b-button>
     </b-row>
 
     <b-row>
-      <p>Status: <span :class="isConnected ? 'text-success' : 'text-danger'">{{isConnected ? 'Connected' : 'Disconnected'}}</span></p>
+      <p>Status: <span :class="isConnected ? 'text-success' : 'text-danger'">{{
+              isConnected ?
+              'Connected' :
+              connectionError ?
+                connectionError :
+                'Disconnected'}}
+            </span>
+      </p>
+    </b-row>
+    <b-row>
+<!--      History-->
+      <b-col cols="2" ></b-col>
     </b-row>
   </div>
 </template>
 
 <script>
-  import {Client} from '@hapi/nes/lib/client';
-export default {
-  name: 'app',
-  components: {
+    import {Client} from '@hapi/nes/lib/client';
 
-  },
-  data() {
-    return{
-      host: '',
-      auth: false,
-      authSettings: {},
-      isConnected: false,
-      history: [],
-      nes: null
-    }
-  },
-  computed: {
-    hostState: () => {return this.host.match(/^ws(s)?:\/\/[\w\d.]*(:[\d]{1,5})?(\/.*)*$/)}
-  },
-  methods: {
-    nesConnect: () => {
-      this.data.nes = new Client(this.data.host);
+    export default {
+        name: 'app',
+        components: {},
+        data() {
+            return {
+                host: 'ws://localhost:8081',
+                auth: false,
+                authSettings: {},
+                isConnected: false,
+                history: [],
+                nes: null,
+                connectionError: null
+            }
+        },
+        computed: {
+            hostState: function () {
+                return this.host.match(/^ws(s)?:\/\/[\w\d.]*(:[\d]{1,5})?(\/.*)*$/) !== null
+            }
+        },
+        methods: {
+            nesConnect: async function () {
+                this.nes = new Client(this.host);
+                let self = this;
 
+                await this.nes.connect().then(() => {
+                    self.isConnected = true;
+                    self.connectionError = null;
+                }).catch(err => {
+                    self.connectionError = err.message;
+                    self.isConnected = false
+                });
+                return true
+            },
+            nesSend() {
+
+            },
+            nesSubscribe() {
+
+            },
+            nesSubscriptionHandler() {
+
+            }
+        }
     }
-  }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
